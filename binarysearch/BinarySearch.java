d1a5e3b9aaa1238c050c24fd92ed916013cc613e
dalang <1160955460@qq.com>
2023-08-29 14:10:05 +0800

    8.29

---
+++ binarysearch/BinarySearch.java
@@ -0,0 +1,45 @@
+package binarysearch;
+
+/**
+ * 基础二分查找两种实现
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 6, 7};
+        System.out.println(binarySearch1(nums, 3));
+    }
+
+    public static int binarySearch1(int[] nums, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch2(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+}
